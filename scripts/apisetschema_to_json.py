8bd43efc75052e37c77ba80f067668f70e9e124a
Mark Jansen <mark.jansen@reactos.org>
2019-11-04 00:27:48 +0100

    Add extra module info Tweak the theme

--- scripts/apisetschema_to_json.py
+++ scripts/apisetschema_to_json.py
@@ -17,40 +17,71 @@ DEFAULTDATADIR = Path(SCRIPTDIR).parent / 'data'
 DEFAULTINPUTDIR = Path(SCRIPTDIR).parent / 'bin'
 DEFAULTCONTENTDIR = Path(SCRIPTDIR).parent / 'content'
 
+def value_or_none(table, name):
+    value = table.entries.get(name, None)
+    if value:
+        return value.decode('UTF-8')
+    return None
 
-def get_version(pe):
-    for root in pe.FileInfo:
-        for stringtables in [fileinfo.StringTable for fileinfo in root if fileinfo.Key == b'StringFileInfo']:
-            for table in stringtables:
-                version = table.entries.get(b'ProductVersion', None)
-                if version:
-                    return version.decode('UTF-8')
+class PeInfo:
+    def __init__(self, pe):
+        self.CompanyName = None
+        self.FileDescription = None
+        self.FileVersion = None
+        self.InternalName = None
+        self.ProductVersion = None
+        self._get_version(pe)
+        arch = pefile.MACHINE_TYPE[pe.FILE_HEADER.Machine]
+        self.arch = arch[len('IMAGE_FILE_MACHINE_'):]
+        # Calculate the md5 from the (internal) raw data of the PE file
+        hasher = md5()
+        hasher.update(pe.__data__)
+        self.md5sum = hasher.hexdigest()
+        self.CheckSum = pe.OPTIONAL_HEADER.CheckSum
+        self.Size = len(pe.__data__)
+
+    def _get_version(self, pe):
+        pe.parse_data_directories(directories=[pefile.DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_RESOURCE']])
+        for root in pe.FileInfo:
+            for stringtables in [fileinfo.StringTable for fileinfo in root if fileinfo.Key == b'StringFileInfo']:
+                for table in stringtables:
+                    self.CompanyName = value_or_none(table, b'CompanyName')
+                    self.FileDescription = value_or_none(table, b'FileDescription')
+                    self.FileVersion = value_or_none(table, b'FileVersion')
+                    self.InternalName = value_or_none(table, b'InternalName')
+                    self.ProductVersion = value_or_none(table, b'ProductVersion')
+
+    def as_json(self):
+        obj = {
+            'Version': '{}-{}'.format(self.ProductVersion, self.arch),
+            'md5': self.md5sum,
+            'InternalName': self.InternalName,
+            'CompanyName': self.CompanyName,
+            'FileDescription': self.FileDescription,
+            'FileVersion': self.FileVersion,
+            'ProductVersion': self.ProductVersion,
+            'PeChecksum': self.CheckSum,
+            'Size': self.Size,
+            'Machine': self.arch
+        }
+        return obj
 
 
 def process_schema_file(input_file, output_dir):
     pe = pefile.PE(input_file)
-
-    arch = pefile.MACHINE_TYPE[pe.FILE_HEADER.Machine]
-    arch = arch[len('IMAGE_FILE_MACHINE_'):]
 
     apiset = next((section for section in pe.sections if section.Name == b'.apiset\x00'), None)
     if not apiset:
         print('No apiset section found in', input_file)
         return
 
-    # Get version from resources
-    version = get_version(pe)
-    if not version:
+    info = PeInfo(pe)
+    if not info.ProductVersion:
         print('No version found in', input_file)
         return
 
-    name = '{}-{}'.format(version, arch)
+    name = '{}-{}'.format(info.ProductVersion, info.arch)
     print('Generating apisetschema for', name)
-
-    # Calculate the md5 from the (internal) raw data of the PE file
-    hasher = md5()
-    hasher.update(pe.__data__)
-    md5sum = hasher.hexdigest()
 
     parser = apisetschema.Parser(apiset.get_data())
     hdr = parser.header()
@@ -71,12 +102,9 @@ def process_schema_file(input_file, output_dir):
             ns['alt_name'] = vals[1]['name']
             ns['alt_host'] = vals[1]['value']
         namespaces.append(ns)
-    obj = {
-        'Version': '{}-{}'.format(version, arch),
-        'md5': md5sum,
-        'namespaces_count': hdr.count,
-        'namespaces': namespaces
-    }
+    obj = info.as_json()
+    obj['namespaces_count'] = hdr.count
+    obj['namespaces'] = namespaces
     filename = '{}.json'.format(name)
     output_dir.mkdir(parents=True, exist_ok=True)
     with open(output_dir / filename, 'w') as json_file:
