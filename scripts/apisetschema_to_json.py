20e1e576764a3694d352a8ce179edfdf340ef592
Mark Jansen <mark.jansen@reactos.org>
2023-12-06 21:51:38 +0100

    Disable apiset generation

--- scripts/apisetschema_to_json.py
+++ scripts/apisetschema_to_json.py
@@ -107,47 +107,49 @@ def process_schema_file(input_file, output_dir):
     obj = info.as_json()
     obj['namespaces_count'] = hdr.count
     obj['namespaces'] = namespaces
-    filename = '{}.json'.format(name)
+    filename = '{}.json'.format(name.lower())
     output_dir.mkdir(parents=True, exist_ok=True)
     with open(output_dir / filename, 'w') as json_file:
         dump(obj, json_file, indent='  ')
     return name
 
 
-def get_forwarders(pe):
-    dirs = [pefile.DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_EXPORT']]
-    pe.parse_data_directories(directories=dirs)
-    exports = []
-    for exp in pe.DIRECTORY_ENTRY_EXPORT.symbols:
-        assert not exp.forwarder
-        if exp.name:
-            assert not exp.forwarder_offset
-        curr_export = {
-            'ord': exp.ordinal
-        }
-        if exp.name:
-            curr_export['name'] = exp.name.decode('utf-8')
-        exports.append(curr_export)
-    return exports
+# def get_forwarders(pe):
+#     dirs = [pefile.DIRECTORY_ENTRY['IMAGE_DIRECTORY_ENTRY_EXPORT']]
+#     pe.parse_data_directories(directories=dirs)
+#     exports = []
+#     for exp in pe.DIRECTORY_ENTRY_EXPORT.symbols:
+#         if exp.forwarder:
+#             assert exp.forwarder.split(b'.')[-1] == exp.name
+#         #assert not exp.forwarder
+#         #if exp.name:
+#         #    assert not exp.forwarder_offset
+#         curr_export = {
+#             'ord': exp.ordinal
+#         }
+#         if exp.name:
+#             curr_export['name'] = exp.name.decode('utf-8')
+#         exports.append(curr_export)
+#     return exports
 
 
-def process_apisets_dir(input_dir, output_dir, version_parent):
-    target_dir = output_dir / version_parent
-    target_dir.mkdir(parents=True, exist_ok=True)
-    for filename in Path(input_dir).glob('**/*.dll'):
-        pe = pefile.PE(filename, fast_load=True)
+# def process_apisets_dir(input_dir, output_dir, version_parent):
+#     target_dir = output_dir / version_parent
+#     target_dir.mkdir(parents=True, exist_ok=True)
+#     for filename in Path(input_dir).glob('**/*.dll'):
+#         pe = pefile.PE(filename, fast_load=True)
 
-        info = PeInfo(pe)
-        if not info.ProductVersion:
-            print('No version found in', input_file)
-            continue
+#         info = PeInfo(pe)
+#         if not info.ProductVersion:
+#             print('No version found in', filename)
+#             continue
 
-        obj = info.as_json()
-        exports = get_forwarders(pe)
-        obj['exports'] = exports
-        output_file = target_dir / '{}.json'.format(filename.stem)
-        with open(output_file, 'w') as json_file:
-            dump(obj, json_file, indent='  ')
+#         obj = info.as_json()
+#         exports = get_forwarders(pe)
+#         obj['exports'] = exports
+#         output_file = target_dir / '{}.json'.format(filename.stem.lower())
+#         with open(output_file, 'w') as json_file:
+#             dump(obj, json_file, indent='  ')
 
 
 class ApisetSource:
@@ -167,11 +169,12 @@ def write_schemas(input_dir, output_dir):
 def main(input_dir, output_dir, content_dir):
     sources = write_schemas(input_dir, output_dir)
     apisets_dir = output_dir / 'apisets'
-    for source in sources:
-        apisets = source.directory / 'apisets'
-        if apisets.is_dir():
-            process_apisets_dir(apisets, apisets_dir, source.version)
-    hugo_pages_from_data.pages_from_data(output_dir, content_dir)
+    #for source in sources:
+    #    apisets = source.directory / 'apisets'
+    #    if apisets.is_dir():
+    #        process_apisets_dir(apisets, apisets_dir, source.version)
+    hugo_pages_from_data.pages_from_schemas(output_dir / 'apisetschema', content_dir)
+    #hugo_pages_from_data.pages_from_apisets(output_dir / 'apisets', content_dir / 'apisets')
 
 
 if __name__ == '__main__':
